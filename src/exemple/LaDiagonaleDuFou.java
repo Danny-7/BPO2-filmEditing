6d21b621382b7aafb226fd6b73e699b71215efb2
Danny-7 <danielaguiar@outlook.fr>
2020-05-17 22:31:27 +0200

    one file deleted

--- src/exemple/LaDiagonaleDuFou.java
+++ src/exemple/LaDiagonaleDuFou.java
@@ -46,7 +46,7 @@ public class LaDiagonaleDuFou implements Film {
         Film filmTest = new Movie(new LineJump());
        Film repeat = Editing.repeat(filmTest, 2);
         repeat = Editing.paste(repeat, film);
-        repeat = Editing.inlay(film, repeat, 200, 5);
+        repeat = Editing.inlay(film, repeat, 54, 5);
         repeat = Editing.frameAMovie(repeat);
         Films.projeter(repeat);
 //        Films.projeter(film);
